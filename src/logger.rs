66df8fae27d2a48ef1dbcafcce315f689057f512
werryxgames <103959882+werryxgames@users.noreply.github.com>
2024-04-21 10:22:59 +0000

    Fixed formatting and added checks (#6)
    * Fixed formatting and added checks
    
    * Fixed warnings

--- src/logger.rs
+++ src/logger.rs
@@ -1,34 +1,35 @@
 pub mod log {
+    #![allow(unused_macros)]
+    #![allow(unused_imports)]
 
-macro_rules! trace {
-    ($str:literal) => (println!(concat!("[TRACE] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[TRACE] ", $str), $($arg)*));
-}
+    macro_rules! trace {
+        ($str:literal) => (println!(concat!("[TRACE] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[TRACE] ", $str), $($arg)*));
+    }
 
-macro_rules! info {
-    ($str:literal) => (println!(concat!("[INFO] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[INFO] ", $str), $($arg)*));
-}
+    macro_rules! info {
+        ($str:literal) => (println!(concat!("[INFO] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[INFO] ", $str), $($arg)*));
+    }
 
-macro_rules! warning {
-    ($str:literal) => (println!(concat!("[WARNING] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[WARNING] ", $str), $($arg)*));
-}
+    macro_rules! warning {
+        ($str:literal) => (println!(concat!("[WARNING] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[WARNING] ", $str), $($arg)*));
+    }
 
-macro_rules! error {
-    ($str:literal) => (println!(concat!("[ERROR] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[ERROR] ", $str), $($arg)*));
-}
+    macro_rules! error {
+        ($str:literal) => (println!(concat!("[ERROR] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[ERROR] ", $str), $($arg)*));
+    }
 
-macro_rules! fatal {
-    ($str:literal) => (println!(concat!("[FATAL] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[FATAL] ", $str), $($arg)*));
-}
+    macro_rules! fatal {
+        ($str:literal) => (println!(concat!("[FATAL] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[FATAL] ", $str), $($arg)*));
+    }
 
-pub(crate) use trace;
-pub(crate) use info;
-pub(crate) use warning;
-pub(crate) use error;
-pub(crate) use fatal;
-
+    pub(crate) use error;
+    pub(crate) use fatal;
+    pub(crate) use info;
+    pub(crate) use trace;
+    pub(crate) use warning;
 }
