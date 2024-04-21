74a081ac03b0e81a4ba1bb08cf19bf1b2542655a
werryxgames <103959882+werryxgames@users.noreply.github.com>
2024-04-21 13:19:39 +0300

    Fixed warnings

--- src/logger.rs
+++ src/logger.rs
@@ -1,29 +1,31 @@
 pub mod log {
+    #![allow(unused_macros)]
+    #![allow(unused_imports)]
 
     macro_rules! trace {
-    ($str:literal) => (println!(concat!("[TRACE] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[TRACE] ", $str), $($arg)*));
-}
+        ($str:literal) => (println!(concat!("[TRACE] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[TRACE] ", $str), $($arg)*));
+    }
 
     macro_rules! info {
-    ($str:literal) => (println!(concat!("[INFO] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[INFO] ", $str), $($arg)*));
-}
+        ($str:literal) => (println!(concat!("[INFO] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[INFO] ", $str), $($arg)*));
+    }
 
     macro_rules! warning {
-    ($str:literal) => (println!(concat!("[WARNING] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[WARNING] ", $str), $($arg)*));
-}
+        ($str:literal) => (println!(concat!("[WARNING] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[WARNING] ", $str), $($arg)*));
+    }
 
     macro_rules! error {
-    ($str:literal) => (println!(concat!("[ERROR] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[ERROR] ", $str), $($arg)*));
-}
+        ($str:literal) => (println!(concat!("[ERROR] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[ERROR] ", $str), $($arg)*));
+    }
 
     macro_rules! fatal {
-    ($str:literal) => (println!(concat!("[FATAL] ", $str)));
-    ($str:literal, $($arg:tt)*) => (println!(concat!("[FATAL] ", $str), $($arg)*));
-}
+        ($str:literal) => (println!(concat!("[FATAL] ", $str)));
+        ($str:literal, $($arg:tt)*) => (println!(concat!("[FATAL] ", $str), $($arg)*));
+    }
 
     pub(crate) use error;
     pub(crate) use fatal;
